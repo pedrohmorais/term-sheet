0594db6a5d47204dd294e021ac3c1042b3c7646d
Pedro <pedro.morais@catho.com>
2026-03-24 01:30:54 -0300

    feat: add provideHttpClient to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(APP_ROUTES, withComponentInputBinding()),
-    provideAnimations()
+    provideAnimations(),
+    provideHttpClient()
   ]
 };
